7e13556c0ea46d0d52e160aea77763e84d202655
Ekode <ekode@protonmail.com>
2020-08-26 16:48:25 +0200

    Added mysql connector and First BDD interfaces

---
+++ src/com/beans/BDD.java
@@ -0,0 +1,33 @@
+package com.beans;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class BDD {
+	
+	private static BDD INSTANCE;
+	
+	Connection conn = null;
+	
+	public BDD() throws SQLException, ClassNotFoundException {
+		String url = "jdbc:mysql://localhost:3306/schoolmaster";
+		String username = "root";
+		String password = "";
+		Class.forName("com.mysql.jdbc.Driver");
+		this.conn = DriverManager.getConnection(url,username,password);
+	}
+	
+	public static BDD getInstance() throws SQLException, ClassNotFoundException {
+		if(INSTANCE == null) {
+			INSTANCE = new BDD();
+		}
+		return INSTANCE;
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+	
+	
+}
