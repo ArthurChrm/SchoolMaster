4fd81f2a03cd357ced89cac73c840ab8058d602d
Ekode <ekode@protonmail.com>
2020-08-27 16:36:12 +0200

    Added permission system for each pages

--- src/com/servlets/Eleves.java
+++ src/com/servlets/Eleves.java
@@ -1,18 +1,29 @@
 package com.servlets;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Eleves extends HttpServlet {
+import com.auth.Roles;
+import com.beans.Personne;
 
+public class Eleves extends HttpServlet {
+	
+	private static List<Roles> autorized = Arrays.asList(Roles.ADMIN,Roles.DIRECTEUR);
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//Redirect if not logged
 		if(req.getSession().getAttribute("user") == null) {resp.sendRedirect(req.getContextPath()+"/home"); return;}
+		
+		//Redirect if not authorized
+		Personne user = (Personne) req.getSession().getAttribute("user");
+		if(!autorized.contains(user.getRole().getEnum())) {resp.sendRedirect(req.getContextPath()+"/home"); return;}
 		
 		this.getServletContext().getRequestDispatcher("/WEB-INF/Eleves.jsp").forward(req, resp);
 	}
